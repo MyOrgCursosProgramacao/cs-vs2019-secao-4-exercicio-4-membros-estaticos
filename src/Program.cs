160063a717fce9671fa51279b1ed4ec2cbb0c519
Diego <alvarodiego@gmail.com>
2020-11-30 19:40:30 -0300

    Programa principal pronto

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace src
 {
@@ -6,7 +7,18 @@ namespace src
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Resolução do Exercício proposto 4 da Seção 4" + Environment.NewLine);
+            Console.WriteLine("Entre com os dados da aquisição de dólares.");
+
+            Console.Write("Valor da cotação do dólar: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Qual a quantidade de dólares a ser adquirida: $ ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write(Environment.NewLine 
+                + "Total a ser pago em reais mais o IOF: R$"
+                + $" {ConversorDeMoeda.DolarParaReal(cotacao,valor).ToString("F2", CultureInfo.InvariantCulture)}"
+                + Environment.NewLine);
+
         }
     }
 }
